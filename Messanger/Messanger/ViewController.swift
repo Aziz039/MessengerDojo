e5b4157157c6315722bdc4a8f676ca13e4644ba7
Abdulaziz Ali <zizogmd@gmail.com>
2022-01-03 23:26:16 +0300

    init project

---
+++ Messanger/Messanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Abdulaziz Alghamdi on 3/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
